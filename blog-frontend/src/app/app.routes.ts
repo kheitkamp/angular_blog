7830fa90f75835d03f742a34070597d0cd9755f1
Kristian Heitkamp <kristian.heitkamp@icloud.com>
2024-07-13 21:47:03 +0000

    add routing

--- blog-frontend/src/app/app.routes.ts
+++ blog-frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { TutorialContentComponent } from './tutorial-content/tutorial-content.component';
+export const routes: Routes = [
 
-export const routes: Routes = [];
+    { path: 'tutorial', component: TutorialContentComponent },
+
+];
