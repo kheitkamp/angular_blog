b67469de0f82139e48903f6207af770f376d5032
Kristian Heitkamp <kristian.heitkamp@icloud.com>
2024-07-13 21:46:32 +0000

    add blog posts component

---
+++ blog-frontend/src/app/blog-posts/blog-posts.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+    @Component({
+      selector: 'app-blog-posts',
+      templateUrl: './blog-posts.component.html',
+      styleUrls: ['./blog-posts.component.scss']
+    })
+    export class BlogPostsComponent implements OnInit {
+
+      constructor() { }
+
+      ngOnInit() { }
+
+    }
