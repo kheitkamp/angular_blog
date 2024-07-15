46f5c289a10f51e1f9537ada50ab8267d387dd06
Kristian Heitkamp <kristian.heitkamp@icloud.com>
2024-07-15 22:22:54 +0000

    add routing for gke tutorial component

--- blog-frontend/src/app/app.routes.ts
+++ blog-frontend/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { AngularTutorial } from './angular-tutorial/angular-tutorial.component';
+import { GkeTutorialComponent } from './gke-tutorial/gke-tutorial.component';
 export const routes: Routes = [
 
-    { path: 'tutorial', component: AngularTutorial },
+    { path: 'angular', component: AngularTutorial },
+
+    { path: 'gke', component: GkeTutorialComponent },
 
 ];
